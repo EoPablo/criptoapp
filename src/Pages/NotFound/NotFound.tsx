aa4b42444ad75e525775fc5cfd18e10d4b056e4a
Pablo <pablocavalcantee@gmail.com>
2024-11-29 10:51:41 -0300

    responsividade pag notfound

--- src/Pages/NotFound/NotFound.tsx
+++ src/Pages/NotFound/NotFound.tsx
@@ -1,9 +1,17 @@
+import styles from './notfound.module.css';
+import Error from '../../assets/error.png';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div>
-        <h1>404</h1>
-        <p>Está Pagina não existe!</p>
+    <div className={styles.container}>
+      <img className={styles.img} src={Error} alt="Imagem de erro" />
+        <h1 className={styles.center}>Opss... Parece que essa página não existe!</h1>
+        <a className={styles.btn}>
+          <Link to='/'>
+            Home
+          </Link>
+        </a>
     </div>
   )
 }
